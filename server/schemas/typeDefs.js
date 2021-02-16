db15ba1bf5517c5e860575d1d0c47527f41799ee
ZMAN <zachh@github.com>
2021-02-16 14:09:49 -0600

    added some user typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,2 +1,11 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: STRING!
+        email: String
+    }
+`;
 
 module.exports = typeDefs;

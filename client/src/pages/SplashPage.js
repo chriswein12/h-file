6043faf9e6d17b31bac27d19c15e61300924ab43
DarrylSimpson <darryl.simpson0793@gmail.com>
2021-02-20 13:43:17 -0600

    splash page renders with react npm start

---
+++ client/src/pages/SplashPage.js
@@ -0,0 +1,35 @@
+// import React from 'react';
+// import '../htmlPageTest/pages.css';
+// import Header from '../components/Header';
+
+// const SplashPage = () => {
+//     return (
+//         <body>
+//             <Header></Header>
+//             <form id="splashPage-form">
+//         <div>
+//             <h1>House-it going?</h1>
+//             <p>At House-it we realize how stressful staying organized as a homeowner can be. 
+//                 Files and documents for your home get thrown in that cluttered box in the basement, 
+//                 never to be seen again...
+//             </p>
+//             <br/>
+//             <p>
+//                 With House-it you can access all of your homeowner filed and information witht the click of a button!
+//             </p>
+//         </div>
+//         <div class="row splashButtons">
+//             <div class="col-6">
+//                 <button type="button" class="btn btn-danger">Sign Up</button>
+//             </div>
+//             <div class="col-6">
+//                 <button type="button" class="btn btn-primary">Login</button>
+//             </div>
+//         </div>
+//     </form>
+//         </body>
+        
+//     )
+// }
+
+// export default SplashPage;

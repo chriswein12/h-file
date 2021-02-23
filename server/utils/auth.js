97240bdee3e0c975f38dd7ef399b5c584bc5fb4f
ZMAN <zachh@github.com>
2021-02-22 20:10:03 -0600

    db password

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 //setting secret and expiration time
-const secret = 'homerepo';
+const secret = 'grouphome28';
 const expiration = '12h';
 
 module.exports = {

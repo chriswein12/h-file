808cc8df08a0711961bca81134c3d0f0bef6917f
DarrylSimpson <darryl.simpson0793@gmail.com>
2021-02-23 19:46:04 -0600

    pushing

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -19,12 +19,24 @@ const Profile = () => {
                 <div className="row pb-5 mb-4">
                     <div className="col-lg-3 col-md-6 mb-4 mb-lg-0">
                         <div className="card shadow-sm border-0 rounded">
-                            <div className="card-body p-0"><img src="../../Assets/splashPage02.jpg" alt="" class="w-100 card-img-top" />
+                            <div className="card-body p-0"><img src="../../../Assets/splashPage02.jpg" alt="" class="w-100 card-img-top" />
                                 <div className="p-4">
                                     <h5 className="mb-2">Home</h5>
                                     <p className="small text-muted mt-1 address">address</p>
                                     <p className="small text-muted mt-1 cityState">milwaukee wi</p>
                                     <p className="small text-muted mt-1 zip">55555</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                        <div className="card shadow-sm border-0 rounded">
+                            <div className="card-body p-0"><img src="../../../Assets/splashPage02.jpg" alt="" class="w-100 card-img-top" />
+                                <div className="p-4">
+                                    <h5 className="mb-2">Home</h5>
+                                    <p className="small text-muted mt-1 address">address</p>
+                                    <p className="small text-muted mt-1 cityState">madison wi</p>
+                                    <p className="small text-muted mt-1 zip">77777</p>
                                 </div>
                             </div>
                         </div>

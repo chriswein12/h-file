444dc433cf9d9aa8cdd9ea5ff64ed6864c29da2f
ZMAN <zachh@github.com>
2021-02-24 11:55:12 -0600

    fixed some issues with buttons and for instead of htmlFor

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -7,29 +7,29 @@ const Signup = (props) => {
         <h1 className="signupHeader">Sign Up</h1>
         <div className="form-floating mb-3">
             <input type="username" className="form-control" id="floatingUsername" placeholder="username"/>
-            <label for="floatingUsername">Username</label>
+            <label htmlFor="floatingUsername">Username</label>
         </div>
         <div className="form-floating mb-3">
             <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-            <label for="floatingInput">Email Address</label>
+            <label htmlFor="floatingInput">Email Address</label>
         </div>
         <div className="form-floating">
             <input type="password" className="form-control"  placeholder="Password"/>
-            <label for="floatingPassword">Password</label>
+            <label htmlFor="floatingPassword">Password</label>
         </div>
         <br/>
         <div className="form-floating">
             <input type="password" className="form-control"  placeholder="Password"/>
-            <label for="floatingPassword2">Confirm Password</label>
+            <label htmlFor="floatingPassword2">Confirm Password</label>
         </div>
         <div className="row">
             <div className=" col-4 submitSignupBtn">
-                <button className="btn btn-danger">Submit</button>
+                <button type="button" className="btn btn-danger">Submit</button>
             </div>
         </div>
         <div className="row signupButtons">
                 <div className="col-4">
-                    <button onClick={props.login} className="btn btn-primary">Already have an account?</button>
+                    <button type="button" onClick={props.login} className="btn btn-primary">Already have an account?</button>
                 </div>
             </div>
     </form>

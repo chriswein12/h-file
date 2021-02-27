66c560453485e18ae04e04f6d572cd86ec52cc6f
Chris Wein <chriswein12@yahoo.com>
2021-02-27 02:23:24 -0600

    updated package.json

--- client/src/App.js
+++ client/src/App.js
@@ -18,15 +18,15 @@ import './App.css';
 
 const client = new ApolloClient({
     request: (operation) => {
-      const token = localStorage.getItem('id_token')
-      operation.setContext({
-        headers: {
-          authorization: token ? `Bearer ${token}` : ''
-        }
-      })
+        const token = localStorage.getItem('id_token')
+        operation.setContext({
+            headers: {
+                authorization: token ? `Bearer ${token}` : ''
+            }
+        })
     },
     uri: '/graphql',
-  })
+})
 
 
 function App() {
